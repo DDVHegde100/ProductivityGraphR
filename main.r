04b56522b81f4ef747a992b507227e7e053fb3ca
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-15 09:13:04 -0400

    Update main.r

--- main.r
+++ main.r
@@ -2,3 +2,5 @@ x <- c(1,2,3,4,5,6,7,8,9,10)
 y <- c(55,48,51,31,47,43,45,38,36,39,35)
 
 plot(x, y, main="Productivity", xlab="Day", ylab="Entertainment Time")
+barplot(y, names.arg = x, density = 30)
+dim(x)
